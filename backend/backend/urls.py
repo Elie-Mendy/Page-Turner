a724d61cb66c029d34ae6eb66878f1a8f4d0ec14
Florine <florine.mendy@gmail.com>
2023-04-25 08:56:23 +0200

    blog app init

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('api/users/', include('base.urls.user_urls')),
     path('api/products/', include('base.urls.product_urls')),
     path('api/orders/', include('base.urls.order_urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
