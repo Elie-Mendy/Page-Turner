35d380de02fe6e053087d3b8b7f6b6916bebc483
Florine <florine.mendy@gmail.com>
2023-04-27 18:33:35 +0200

    implement api blog

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path 
-from blog.views import BlogHome, BlogPostCreate, BlogPostUpdate, BlogPostDetail, BlogPostDelete
-from django.contrib.auth.decorators import login_required
+from django.urls import path
+from blog import views 
 
-app_name = "posts" #définir un espace de noms -- le nom des urls
-
-urlpatterns = [   
-    path('', BlogHome.as_view(), name='home'), 
-    path('create', login_required(BlogPostCreate.as_view()), name='create'),     
-    path('edit/<str:slug>/', login_required(BlogPostUpdate.as_view()), name='edit'), 
-    path('<str:slug>/', BlogPostDetail.as_view(), name='post'),   
-    path('delete/<str:slug>/', login_required(BlogPostDelete.as_view()), name='delete'),   
+urlpatterns = [    
+    path('', views.getPosts, name="posts"), 
+    path('<str:pk>', views.getPost, name="post"), 
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,54 +1,19 @@
-from django.shortcuts import render
-from django.views.generic import ListView # permet de récupérer toutes les données du modèle 
-from django.views.generic import CreateView # permet de créer une vue
-from django.views.generic import UpdateView # permet de créer une vue
-from django.views.generic import DetailView # permet de créer une vue
-from django.views.generic import DeleteView # permet de créer une vue
 from blog.models import BlogPost
-from django.http import HttpResponse
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from blog.serializers import BlogPostSerializer
+
+@api_view(['GET'])
+def getPosts(request):
+    posts = BlogPost.objects.all().order_by("-created_on")
+    serializer = BlogPostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 
-
-class BlogHome(ListView) :
-  model = BlogPost      # récupère tous les articles du blog crées dans notre modèle BlogHome
-  context_object_name = "posts" # spécifie un nom pour la var à utiliser à l'intérieur du fichier du template
-  template_name = "posts/blogpost_list.html"
-
-  # ajouter des modifications dans notre template
-  def get_queryset(self):
-    queryset = super().get_queryset()
-    if self.request.user.is_authenticated:
-      return queryset
-    return queryset.filter(published=True)
-  
-  
-@method_decorator(login_required, name="dispatch")
-class BlogPostCreate(CreateView) :
-  model = BlogPost      # récupère tous les articles du blog crées dans notre modèle BlogHome
-  template_name = "posts/blogpost_create.html"
-  fields = ['title', 'content',]
-
-
-@method_decorator(login_required, name="dispatch")
-class BlogPostUpdate(UpdateView) :
-  model = BlogPost      # récupère tous les articles du blog crées dans notre modèle BlogHome
-  context_object_name = "post" # éviter d'utiliser la var de base object
-  template_name = "posts/blogpost_edit.html"
-  fields = ['title', 'content', 'published']
-
-
-class BlogPostDetail(DetailView) :
-  model = BlogPost      # récupère tous les articles du blog crées dans notre modèle BlogHome
-  context_object_name = "post" # éviter d'utiliser la var de base object
-  template_name = "posts/blogpost_detail.html"
-
-
-@method_decorator(login_required, name="dispatch")
-class BlogPostDelete(DeleteView) :
-  model = BlogPost      # récupère tous les articles du blog crées dans notre modèle BlogHome
-  context_object_name = "post" # éviter d'utiliser la var de base object
-  success_url = reverse_lazy("posts:home")
-  template_name = "posts/blogpost_confirm_delete.html"
+@api_view(['GET'])
+def getPost(request, pk):
+    post = BlogPost.objects.get(id=pk)
+    serializer = BlogPostSerializer(post, many=False)
+    return Response(serializer.data)
