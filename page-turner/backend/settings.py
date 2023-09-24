c8878a5c88c078b1cb16f220892e1183b8f32793
Elie MENDY <emendy@efficity.com>
2023-09-24 14:40:50 +0200

    setting AWS database RDS

--- page-turner/backend/settings.py
+++ page-turner/backend/settings.py
@@ -133,8 +133,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'pageturner',
+        "USER": 'postgres',
+        "PASSWORD": 'Zefy3173',
+        "HOST": 'page-turner-db.cgrk4cx2leph.us-east-1.rds.amazonaws.com',
+        "PORT": '5432',
     }
 }
 
